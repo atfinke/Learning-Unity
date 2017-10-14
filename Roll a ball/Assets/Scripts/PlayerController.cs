684f9f9676ae882467fb01290a163d53b370d0f1
Andrew Finke <andrewfinke@me.com>
2017-10-14 14:20:11 -0500

    .

---
+++ Roll a ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+	public float speed;
+	public Text countText;
+
+	private Rigidbody rb;
+	private int count;
+
+	void Start ()
+	{
+		Application.targetFrameRate = 60;
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		countText.text = "Count: " + count.ToString ();
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+		rb.AddForce (movement * speed);
+
+		Vector3 acc = Input.acceleration;
+		rb.AddForce(acc.x * speed, 0, (acc.y + 0.5f) * speed);
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive (false);
+			count += 1;
+			countText.text = "Count: " + count.ToString ();
+		}
+	}
+
+}
